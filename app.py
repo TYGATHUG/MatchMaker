4a65658d2036027e378ad643be79af4b87bed769
Ben Diep <ben_diep@hotmail.com>
2017-09-02 11:44:03 +1000

    got rid of register/login pages

--- app.py
+++ app.py
@@ -64,15 +64,6 @@ def member():
 
     return render_template('member.html')
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
 
 @app.route('/profile', methods=['GET', 'POST'])
 def profile():
--- static/js/main.js
+++ static/js/main.js
@@ -66,4 +66,60 @@ $(document).ready(function() {
 	});
 
 
+	// Form Modals
+	function loadModalAnimation() {
+        var rmodal = document.getElementById('register-modal'); // Get the modal
+        var rbtn = document.getElementById('register-btn'); // Get the button that opens the modal
+        var rspan = document.getElementById('register-close'); // Get the <span> element that closes the modal
+
+		var lmodal = document.getElementById('login-modal');
+        var lbtn = document.getElementById('login-btn');
+        var lspan = document.getElementById('login-close');
+
+        // When the user clicks on the button, open the modal
+        rbtn.onclick = function () {
+        	lmodal.style.display = "none";
+            rmodal.style.display = "block";
+
+            $(rmodal)
+                .css('opacity', 0)
+                .slideDown('slow')
+                .animate(
+                    {opacity: 1},
+                    {queue: false, duration: 'fast'}
+                );
+        }
+        lbtn.onclick = function () {
+        	rmodal.style.display = "none";
+            lmodal.style.display = "block";
+
+            $(lmodal)
+                .css('opacity', 0)
+                .slideDown('slow')
+                .animate(
+                    {opacity: 1},
+                    {queue: false, duration: 'fast'}
+                );
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        rspan.onclick = function () {
+			$(rmodal).fadeOut('fast');
+        }
+        lspan.onclick = function () {
+			$(lmodal).fadeOut('fast');
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+		window.onclick = function (event) {
+            if (event.target == rmodal) {
+                $(rmodal).fadeOut('fast');
+            }
+        	if (event.target == lmodal) {
+            	$(lmodal).fadeOut('fast');
+            }
+        }
+    };
+	$(window).onload=loadModalAnimation();
+
 });
