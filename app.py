de044583bc70a9dd162ef706ac04a0c32755ca14
A-Cheong <A-Cheong@users.noreply.github.com>
2017-09-06 06:09:21 +1000

    Add images to dummy data
    Images have been resized appropriately

--- app.py
+++ app.py
@@ -89,6 +89,8 @@ class Match(db.Model):
     reading = db.Column(db.Integer())
 
     # create a python object of Match
+    # keep here for now, I will delete later if it's useless - alex
+
     def __init__(self, username, view_count, image, name, gender, age, height, location, education,
                  ethnicity, religion, bio, practicality, love, excitment, challenge, closeness,
                  structure, live_music, spare_moment_purchases, gym_member, outdoors, volunteering,
@@ -118,32 +120,6 @@ class Match(db.Model):
         self.volunteering = volunteering
         self.entreprenuer = entreprenuer
         self.reading = reading
-
-def seedusers():
-
-    users = [
-        ("Stefanie", "4.jpeg", "Stefanie", "Female", 30, 170, "Sydney", "Bachelor", "Hispanic", "Catholic",
-         "I'm just you're average person.", 5, 20, 15, 43, 2, 75, 0.5, 0.5, 1, 0, 0, 1, 0.5),
-        ("Tom", "5.jpeg", "Tom", "Male", 28, 150, "Syndey", "Associate Degree", "White", "Catholic",
-         "I like to surf and long walks.", 10, 15, 3, 52, 12, 32, 1, 1, 0, 0, 0.5, 1, 0),
-        ("Arwa", "6.jpeg", "Arwa", "Female", 25, 180, "Sydney", "PHD", "African", "Atheist", "I study a lot.", 43, 59,
-         2, 23, 3, 68, 0, 0.5, 1, 1, 1, 0, 0.5),
-        ("John", "7.jpeg", "John", "Male", 30, 190, "Melbourne", "Bachelor", "Asian", "Atheist", "I like to eat.", 10,
-         54, 32, 2, 42, 4, 1, 1, 0.5, 1, 0, 0, 1),
-        ("Ashe", "8.jpeg", "Ashe", "Male", 22, 175, "Melbourne", "Cert IV", "White", "Christian",
-         "I like to go bowling.", 2, 32, 76, 8, 23, 22, 1, 0, 0, 0.5, 1, 1, 1),
-        ("Sophie", "9.jpeg", "Sophie", "Female", 39, 180, "Melbourne", "Bachelor", "White", "Catholic",
-         "I like to go fishing.", 3, 5, 21, 42, 74, 29, 1, 1, 0.5, 1, 0, 0, 1),
-        ("Laura", "10.jpeg", "Laura", "Female", 29, 177, "Melbourne", "Bachelor", "Asian", "Atheist",
-         "I like to dance.", 22, 5, 75, 34, 6, 43, 1, 0.5, 0.5, 0, 1, 1, 1)
-    ]
-
-    con = sqlite3.connect(db)
-
-    con.executemany("INSERT INTO Match(username, image, name, gender, age, height, location, education, ethnicity, "
-                    "religion, bio, practicality, love, excitment, challenge, closeness, structure, live_music, "
-                    "spare_moment_purchases, gym_member, outdoors, volunteering, entreprenuer, reading) "
-                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", users)
 
 # ---------------------------------------------------------------------------------
 #   Classes Forms
